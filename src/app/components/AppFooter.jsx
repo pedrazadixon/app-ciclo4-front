9d6c86c4d95748f832045762145e7c320939f4b7
Dixon Pedraza <pedrazadixon@gmail.com>
2021-11-22 15:17:35 -0500

    avances

--- src/app/components/AppFooter.jsx
+++ src/app/components/AppFooter.jsx
@@ -3,9 +3,9 @@ import 'sass/example.scss';
 class AppFooter extends React.Component {
   render() {
     return (
-			<footer class="footer">
-				<div class="container">
-					<span class="text-muted">Rights reserved to Yamen Sharaf and stuff</span>
+			<footer className="footer">
+				<div className="container">
+					<span className="text-muted">Rights reserved to Yamen Sharaf and stuff</span>
 				</div>
 			</footer>
     );
