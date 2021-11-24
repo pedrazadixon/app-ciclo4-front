ac25cba662a120402afddc9bc6c7a292b94270f1
Dixon Pedraza <pedrazadixon@gmail.com>
2021-11-23 22:41:55 -0500

    avances header

--- src/app/components/AppNavbar.jsx
+++ src/app/components/AppNavbar.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMapMarkedAlt,
@@ -7,66 +7,89 @@ import {
   faUsers,
 } from "@fortawesome/free-solid-svg-icons";
 
-class AppNavbar extends React.Component {
-  render() {
-    return (
-      <header className="p-3 bg-dark text-white">
-        <div className="container">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <img
-              src={process.env.PUBLIC_URL + "/img/logo_misiontic.png"}
-              height="54px"
-            />
+function AppNavbar() {
+  let location = useLocation();
 
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 ms-lg-4">
-              <li>
-                <a href="##" className="nav-link text-secondary">
-                  <FontAwesomeIcon
-                    icon={faCopy}
-                    size="2x"
-                    className="d-block mx-auto mb-1"
-                  />
-                  Ordenes
-                </a>
-              </li>
-              <li>
-                <a href="##" className="nav-link text-white">
-                  <FontAwesomeIcon
-                    icon={faMapMarkedAlt}
-                    size="2x"
-                    className="d-block mx-auto mb-1"
-                  />
-                  Destinos
-                </a>
-              </li>
-              <li>
-                <a href="##" className="nav-link text-white">
-                  <FontAwesomeIcon
-                    icon={faUsers}
-                    size="2x"
-                    className="d-block mx-auto mb-1"
-                  />
-                  Usuarios
-                </a>
-              </li>
-            </ul>
+  return (
+    <header className="p-3 bg-dark text-white">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <img
+            src={process.env.PUBLIC_URL + "/img/logo_misiontic.png"}
+            height="54px"
+            alt="logo"
+          />
 
-            <div>
-              <p className="my-0 me-3">Nombre Apellido (Rol)</p>
-            </div>
-
-            <div className="text-end">
-              <Link to="/iniciar-sesion">
-                <button type="button" className="btn btn-outline-light">
-                  Cerrar Sesion
-                </button>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 ms-lg-4">
+            <li>
+              {/* <Link to="/ordenes" className="nav-link text-secondary"> */}
+              <Link
+                to="/ordenes"
+                className={`nav-link ${
+                  location.pathname.includes("ordenes")
+                    ? "text-secondary"
+                    : "text-white"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faCopy}
+                  size="2x"
+                  className="d-block mx-auto mb-1"
+                />
+                Ordenes
               </Link>
-            </div>
+            </li>
+            <li>
+              <Link
+                to="/destinos"
+                className={`nav-link ${
+                  location.pathname.includes("destinos")
+                    ? "text-secondary"
+                    : "text-white"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faMapMarkedAlt}
+                  size="2x"
+                  className="d-block mx-auto mb-1"
+                />
+                Destinos
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/usuarios"
+                className={`nav-link ${
+                  location.pathname.includes("usuarios")
+                    ? "text-secondary"
+                    : "text-white"
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faUsers}
+                  size="2x"
+                  className="d-block mx-auto mb-1"
+                />
+                Usuarios
+              </Link>
+            </li>
+          </ul>
+
+          <div>
+            <p className="my-0 me-3">Nombre Apellido (Rol)</p>
+          </div>
+
+          <div className="text-end">
+            <Link to="/iniciar-sesion">
+              <button type="button" className="btn btn-outline-light">
+                Cerrar Sesion
+              </button>
+            </Link>
           </div>
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
 }
 
 export default AppNavbar;
--- src/app/components/Error404.jsx
+++ src/app/components/Error404.jsx
@@ -1,15 +1,13 @@
-import * as React from 'react';
+import * as React from "react";
 class Error404 extends React.Component {
   render() {
     return (
       <div className="container">
         <div className="row mt-5">
           <div className="col-12">
-            <h1 className="display-1 text-danger text-center">
-              404
-            </h1>
+            <h1 className="display-1 text-danger text-center">404</h1>
             <h1 className="display-4 text-danger text-center">
-              Page not found
+              Página no encontrada
             </h1>
           </div>
         </div>
