24732bce3c60860f9f922a8720e1abca5868ecab
Dixon Pedraza <pedrazadixon@gmail.com>
2021-12-01 23:27:34 -0500

    injecto header user id en cada request de axios

--- src/app/utils/axios.js
+++ src/app/utils/axios.js
@@ -4,4 +4,17 @@ const axiosInstance = axios.create({
   // baseURL: "https://some-domain.com/api/",
 });
 
+axiosInstance.interceptors.request.use(
+  function (config) {
+    let usuario = JSON.parse(localStorage.getItem("usuario"));
+    if (usuario) {
+      config.headers["User-Id"] = usuario._id;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
