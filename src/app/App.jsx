baae3612027ca5efa17b1749ea3e49ed77091e59
Dixon Pedraza <pedrazadixon@gmail.com>
2021-12-01 19:50:18 -0500

    mejoras en toast

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <div id="App">
-        <ToastContainer />
+        <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
         <BrowserRouter>
           <Switch>
             <Route exact path="/">
